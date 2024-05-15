f5c2ddf63f257aa98445d4c2ded7ae52debed0b3
SANTHOSHA K <126556160+Santhoshak1999@users.noreply.github.com>
2024-05-15 21:43:17 +0530

    Add files via upload

---
+++ LinkedList/src/App.java
@@ -0,0 +1,14 @@
+
+public class App {
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.insert(12);
+        list.insert(23);
+        list.insert(25);
+        list.insert(100);
+        list.show();
+
+
+
+    }
+}
